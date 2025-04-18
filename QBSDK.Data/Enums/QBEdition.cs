ea841cbc34dd1e6446a474634529b552f59d283c
travis@travestyofcode.com <travis@travestyofcode.com>
2025-04-18 16:28:29 -0400

    Adds QBVersion to track major, minor and editions of QB along with checks to see if a version is supported against another version.

---
+++ QBSDK.Data/Enums/QBEdition.cs
@@ -0,0 +1,10 @@
+﻿namespace QBSDK.Data;
+
+[Flags]
+public enum QBEdition
+{
+    US = 1 << 0,
+    CA = 1 << 1,
+    UK = 1 << 2,
+    ALL = US | CA | UK
+}
---
+++ QBSDK.Data/QBVersion.cs
@@ -0,0 +1,39 @@
+﻿namespace QBSDK.Data;
+
+public record QBVersion(int MajorVersion = 13, int MinorVersion = 0, QBEdition QBEdition = QBEdition.ALL)
+{
+    public bool Supports(QBVersion version)
+    {
+        if (version.MajorVersion < MajorVersion)
+        {
+            return QBEdition.HasFlag(version.QBEdition);
+        }
+        if (version.MajorVersion > MajorVersion)
+        {
+            return false;
+        }
+        return version.MinorVersion <= MinorVersion && QBEdition.HasFlag(version.QBEdition);
+    }
+    public bool Supports(int majorVersion) => MajorVersion >= majorVersion;
+    public bool Supports(int majorVersion, int minorVersion)
+    {
+        if (majorVersion < MajorVersion)
+        {
+            return true;
+        }
+        return minorVersion <= MinorVersion;
+    }
+    public bool Supports(int majorVersion, QBEdition edition) => MajorVersion >= majorVersion && QBEdition.HasFlag(edition);
+    public bool Supports(int majorVersion, int minorVersion, QBEdition edition)
+    {
+        if (majorVersion < MajorVersion)
+        {
+            return QBEdition.HasFlag(edition);
+        }
+        if (majorVersion > MajorVersion)
+        {
+            return false;
+        }
+        return minorVersion <= MinorVersion && QBEdition.HasFlag(edition);
+    }
+}
