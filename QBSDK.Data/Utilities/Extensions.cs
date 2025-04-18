e44dcffd45b669ce0c8c6d756c264aafa0fefc37
travis@travestyofcode.com <travis@travestyofcode.com>
2025-04-18 15:53:01 -0400

    Adds string null check extensions.

---
+++ QBSDK.Data/Utilities/Extensions.cs
@@ -0,0 +1,7 @@
+﻿namespace QBSDK.Data.Utilities;
+
+public static class Extensions
+{
+    public static bool IsNullOrWhiteSpace(this string? value) => string.IsNullOrWhiteSpace(value);
+    public static bool IsNotNullOrWhiteSpace(this string? value) => !string.IsNullOrWhiteSpace(value);
+}
