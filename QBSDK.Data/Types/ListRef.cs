5fe6b6a359bee7b7c7edd44e1c28378a8dec7a7f
travis@travestyofcode.com <travis@travestyofcode.com>
2025-04-19 05:53:50 -0400

    Adds ListRef class to be used for Add/Mod/Query requests, along with extensions to assist building XElements.

---
+++ QBSDK.Data/Types/ListRef.cs
@@ -0,0 +1,49 @@
+﻿using QBSDK.Data.Utilities;
+using System.Runtime.CompilerServices;
+using System.Xml.Linq;
+
+namespace QBSDK.Data;
+
+public class ListRef
+{
+    public string? ListID { get; set; }
+    public string? FullName { get; set; }
+
+    public ListRef() { }
+    public ListRef(string? listID, string? fullName)
+    {
+        ListID = listID;
+        FullName = fullName;
+    }
+
+    public XElement ToQBXML(QBVersion version = default, string name = nameof(ListRef))
+    {
+        return new XElement(name)
+            .Append(ListID)
+            .Append(FullName);
+    }
+}
+
+public static class ListRefExtensions
+{
+    public static ListRef? AsListRef(this ListRefBase? entity) => entity == null
+            ? null
+            : new ListRef(entity.ListID, entity.FullName);
+
+    public static XElement Append(this XElement element, ListRef? value, [CallerArgumentExpression(nameof(value))] string name = "", QBVersion version = default)
+    {
+        if (value != null)
+        {
+            element.Add(value.ToQBXML(version, name));
+        }
+        return element;
+    }
+    public static XElement AppendIf(this XElement element, Func<bool> predicate, ListRef? value, [CallerArgumentExpression(nameof(value))] string name = "", QBVersion version = default)
+    {
+        if (predicate() && value != null)
+        {
+            element.Add(value.ToQBXML(version, name));
+        }
+        return element;
+    }
+}
---
+++ QBSDK.Data/Utilities/XmlExtensions.cs
@@ -0,0 +1,65 @@
+﻿using System.Runtime.CompilerServices;
+using System.Xml.Linq;
+
+namespace QBSDK.Data.Utilities;
+
+public static class XmlExtensions
+{
+    public static XElement Append(this XElement element, string? value, [CallerArgumentExpression(nameof(value))] string name = "")
+    {
+        if (value != null)
+        {
+            element.Add(new XElement(name, value.ToString()));
+        }
+        return element;
+    }
+    public static XElement Append(this XElement element, bool? value, [CallerArgumentExpression(nameof(value))] string name = "")
+    {
+        if (value != null)
+        {
+            element.Add(new XElement(name, value.Value ? "true" : "false"));
+        }
+        return element;
+    }
+    public static XElement Append(this XElement element, int? value, [CallerArgumentExpression(nameof(value))] string name = "")
+    {
+        if (value != null)
+        {
+            element.Add(new XElement(name, value.ToString()));
+        }
+        return element;
+    }
+    public static XElement Append(this XElement element, decimal? value, int decimalPlaces = 2, [CallerArgumentExpression(nameof(value))] string name = "")
+    {
+        if (value != null)
+        {
+            element.Add(new XElement(name, value.Value.ToString($"F{decimalPlaces}")));
+        }
+        return element;
+    }
+    public static XElement Append(this XElement element, Enum? value, [CallerArgumentExpression(nameof(value))] string name = "")
+    {
+        if (value != null)
+        {
+            element.Add(new XElement(name, value.ToString()));
+        }
+        return element;
+    }
+    public static XElement Append(this XElement element, DateOnly? value, [CallerArgumentExpression(nameof(value))] string name = "")
+    {
+        if (value != null)
+        {
+            element.Add(new XElement(name, value.Value.ToString("yyyy-MM-dd")));
+        }
+        return element;
+    }
+
+    public static XElement AppendIf(this XElement element, Func<bool> predicate, int? value, [CallerArgumentExpression(nameof(value))] string name = "")
+    {
+        if (predicate() && value != null)
+        {
+            element.Add(new XElement(name, value.ToString()));
+        }
+        return element;
+    }
+}
