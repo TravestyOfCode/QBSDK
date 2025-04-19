27f0ed601a2c16eb65eaf32cd6a18dbe0bc79f59
travis@travestyofcode.com <travis@travestyofcode.com>
2025-04-19 05:54:12 -0400

    Adds AccountAdd with methods to convert from Account and to generate QB XElements.

---
+++ QBSDK.Data/Lists/AccountAdd.cs
@@ -0,0 +1,67 @@
+﻿using QBSDK.Data.Utilities;
+using System.Xml.Linq;
+
+namespace QBSDK.Data;
+
+public class AccountAdd
+{
+    public int Id { get; set; }
+    public int? StatusCode { get; set; }
+    public string? StatusSeverity { get; set; }
+    public string? StatusMessage { get; set; }
+
+    public required string Name { get; set; }
+    public bool? IsActive { get; set; }
+    public ListRef? ParentRef { get; set; }
+    public AccountType? AccountType { get; set; }
+    public string? AccountNumber { get; set; }
+    public string? BankNumber { get; set; }
+    public string? Desc { get; set; }
+    public decimal? OpenBalance { get; set; }
+    public DateOnly? OpenBalanceDate { get; set; }
+    public ListRef? SalesTaxCodeRef { get; set; }
+    public int? TaxLineID { get; set; }
+    public ListRef? CurrencyRef { get; set; }
+
+    public XElement ToQBXML(QBVersion version = default, string name = nameof(AccountAdd))
+    {
+        return new XElement("AccountAddRq", new XElement(name)
+            .Append(Name)
+            .Append(IsActive)
+            .Append(ParentRef)
+            .Append(AccountType)
+            .Append(AccountNumber)
+            .Append(BankNumber)
+            .Append(Desc)
+            .Append(OpenBalance)
+            .Append(OpenBalanceDate)
+            .AppendIf(() => version.Supports(6, QBEdition.CA | QBEdition.UK), SalesTaxCodeRef)
+            .AppendIf(() => version.Supports(7, QBEdition.US | QBEdition.CA), TaxLineID)
+            .AppendIf(() => version.Supports(8), CurrencyRef));
+    }
+
+}
+
+public static class AccountAddExtensions
+{
+    public static AccountAdd AsAdd(this Account entity)
+    {
+        ArgumentNullException.ThrowIfNull(entity);
+        ArgumentNullException.ThrowIfNull(entity.Name);
+        ArgumentNullException.ThrowIfNull(entity.AccountType);
+
+        return new AccountAdd()
+        {
+            Name = entity.Name,
+            IsActive = entity.IsActive,
+            ParentRef = entity.ParentRef.AsListRef(),
+            AccountType = entity.AccountType,
+            AccountNumber = entity.AccountNumber,
+            BankNumber = entity.BankNumber,
+            Desc = entity.Desc,
+            //SalesTaxCodeRef = entity.SalesTaxCodeRef.AsListRef(),
+            TaxLineID = entity.TaxLineInfo?.TaxLineID,
+            //CurrencyRef = entity.CurrencyRef.AsListRef()
+        };
+    }
+}
