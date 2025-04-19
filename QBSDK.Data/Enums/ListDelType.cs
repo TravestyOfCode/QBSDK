276362cf1ef1939a5c27bff1af2a1a783c977f14
travis@travestyofcode.com <travis@travestyofcode.com>
2025-04-19 06:04:08 -0400

    Adds AccountDel class with related enum.

---
+++ QBSDK.Data/Enums/ListDelType.cs
@@ -0,0 +1,6 @@
+﻿namespace QBSDK.Data;
+
+public enum ListDelType
+{
+    Account, BillingRate, Class, Currency, Customer, CustomerMsg, CustomerType, DateDrivenTerms, Employee, InventorySite, ItemDiscount, ItemFixedAsset, ItemGroup, ItemInventory, ItemInventoryAssembly, ItemNonInventory, ItemOtherCharge, ItemPayment, ItemSalesTax, ItemSalesTaxGroup, ItemService, ItemSubtotal, JobType, OtherName, PaymentMethod, PayrollItemNonWage, PayrollItemWage, PriceLevel, SalesRep, SalesTaxCode, ShipMethod, StandardTerms, ToDo, UnitOfMeasureSet, Vehicle, Vendor, VendorType, WorkersCompCode
+}
---
+++ QBSDK.Data/Lists/AccountDel.cs
@@ -0,0 +1,38 @@
+﻿using QBSDK.Data.Utilities;
+using System.Xml.Linq;
+
+namespace QBSDK.Data;
+
+// The QBSDK uses ListDel for all lists. This is just a wrapper to keep the
+// naming convention.
+public class AccountDel
+{
+    public int Id { get; set; }
+    public int? StatusCode { get; set; }
+    public string? StatusSeverity { get; set; }
+    public string? StatusMessage { get; set; }
+
+    public ListDelType ListDelType { get; } = ListDelType.Account;
+    public required string ListID { get; set; }
+
+    public XElement ToQBXML(QBVersion version = default, string name = "ListDelRq")
+    {
+        return new XElement(name)
+            .Append(ListDelType)
+            .Append(ListID);
+    }
+}
+
+public static class AccountDelExtensions
+{
+    public static AccountDel AsDel(this Account entity)
+    {
+        ArgumentNullException.ThrowIfNull(entity);
+        ArgumentNullException.ThrowIfNull(entity.ListID);
+
+        return new AccountDel()
+        {
+            ListID = entity.ListID
+        };
+    }
+}
