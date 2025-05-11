675615dffc5bdbfc9a0d7119e16ce4f732fe9af1
travis@travestyofcode.com <travis@travestyofcode.com>
2025-05-10 21:12:37 -0400

    Changes set functions to use out parameters so the values get saved.

--- QBSDK/Utilities/XAttributeExtensions.cs
+++ QBSDK/Utilities/XAttributeExtensions.cs
@@ -22,15 +22,15 @@ internal static class XAttributeExtensions
         return element;
     }
 
-    public static void SetFromAttribute(this XElement? element, long? value, [CallerArgumentExpression(nameof(value))] string name = "")
+    public static void SetFromAttribute(this XElement? element, out long? value, [CallerArgumentExpression(nameof(value))] string name = "")
     {
         value = long.TryParse(element?.Attribute(name)?.Value, out var result) ? result : null;
     }
-    public static void SetFromAttribute(this XElement? element, int? value, [CallerArgumentExpression(nameof(value))] string name = "")
+    public static void SetFromAttribute(this XElement? element, out int? value, [CallerArgumentExpression(nameof(value))] string name = "")
     {
         value = int.TryParse(element?.Attribute(name)?.Value, out var result) ? result : null;
     }
-    public static void SetFromAttribute(this XElement? element, string? value, [CallerArgumentExpression(nameof(value))] string name = "")
+    public static void SetFromAttribute(this XElement? element, out string? value, [CallerArgumentExpression(nameof(value))] string name = "")
     {
         value = element?.Attribute(name)?.Value;
     }
