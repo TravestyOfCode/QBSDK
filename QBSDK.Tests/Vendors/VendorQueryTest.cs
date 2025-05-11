2db13f4361cba33f6fb835baec8222750282e25b
travis@travestyofcode.com <travis@travestyofcode.com>
2025-05-10 21:15:22 -0400

    Adds tests for vendor query.

--- QBSDK.Tests/Vendors/VendorQueryTest.cs
+++ QBSDK.Tests/Vendors/VendorQueryTest.cs
@@ -1,10 +1,38 @@
-﻿namespace QBSDK.Tests.Vendors;
+﻿using Microsoft.Extensions.Logging;
+using Xunit.Abstractions;
 
-public class VendorQueryTest
+namespace QBSDK.Tests.Vendors;
+
+public class VendorQueryTest(ITestOutputHelper output)
 {
     [Fact]
     public void VendorQueryProducesCorrectXElement()
     {
-        VendorQueryRq
+        var rq = new VendorQueryRq()
+        {
+            ListID = ["ABc123"]
+        };
+
+        var result = rq.ToXElement();
+
+        Assert.Equal(nameof(VendorQueryRq), result.Name.LocalName);
+        Assert.Null(result.Element("MaxReturned"));
+        Assert.NotNull(result.Element("ListID"));
+
+        output.WriteLine(result.ToString());
+
+    }
+
+    [Fact]
+    public void VendorQueryProcessRequest()
+    {
+        var loggerFactory = LoggerFactory.Create(c => c.SetMinimumLevel(LogLevel.Debug));
+        var conn = new QBConnection(loggerFactory.CreateLogger<QBConnection>());
+        var rq = new VendorQueryRq();
+
+        conn.ProcessRequest(rq);
+
+        Assert.Equal(0, rq.StatusCode);
+
     }
 }
