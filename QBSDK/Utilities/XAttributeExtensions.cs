163338b5364f825c7ae4fe9e7847c1532060d971
travis@travestyofcode.com <travis@travestyofcode.com>
2025-05-10 20:26:06 -0400

    Adds additional AddAttribute fucntions for string and enums. Removes out parameters.

--- QBSDK/Utilities/XAttributeExtensions.cs
+++ QBSDK/Utilities/XAttributeExtensions.cs
@@ -5,15 +5,32 @@ namespace QBSDK.Utilities;
 
 internal static class XAttributeExtensions
 {
-    public static void SetFromAttribute(this XElement? element, out long? value, [CallerArgumentExpression(nameof(value))] string name = "")
+    public static XElement AddAttribute(this XElement element, string? value, [CallerArgumentExpression(nameof(value))] string name = "")
+    {
+        if (value != null)
+        {
+            element.Add(new XAttribute(name, value));
+        }
+        return element;
+    }
+    public static XElement AddAttribute<T>(this XElement element, T? value, [CallerArgumentExpression(nameof(value))] string name = "") where T : struct, Enum
+    {
+        if (value != null)
+        {
+            element.Add(new XAttribute(name, value.Value.ToString()));
+        }
+        return element;
+    }
+
+    public static void SetFromAttribute(this XElement? element, long? value, [CallerArgumentExpression(nameof(value))] string name = "")
     {
         value = long.TryParse(element?.Attribute(name)?.Value, out var result) ? result : null;
     }
-    public static void SetFromAttribute(this XElement? element, out int? value, [CallerArgumentExpression(nameof(value))] string name = "")
+    public static void SetFromAttribute(this XElement? element, int? value, [CallerArgumentExpression(nameof(value))] string name = "")
     {
         value = int.TryParse(element?.Attribute(name)?.Value, out var result) ? result : null;
     }
-    public static void SetFromAttribute(this XElement? element, out string? value, [CallerArgumentExpression(nameof(value))] string name = "")
+    public static void SetFromAttribute(this XElement? element, string? value, [CallerArgumentExpression(nameof(value))] string name = "")
     {
         value = element?.Attribute(name)?.Value;
     }
